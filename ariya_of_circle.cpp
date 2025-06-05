c223bd76e5ebd944126ae0d105f0d4a3e950e634
Mehedi hasan Foysal <121812709+mehedihasan8@users.noreply.github.com>
2025-06-05 14:42:52 +0000

    Area of a Circle

---
+++ ariya_of_circle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    double a;
+    cin >> a;
+    
+    cout << fixed << setprecision(9);
+    
+    double  power =  (3.141592653) * pow(a, 2);
+    
+    cout << power;
+
+    return 0;
+}
