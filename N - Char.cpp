4db73131ab74b13bfaa6d86fc81ac28ec4e4b466
Mehedi hasan Foysal <121812709+mehedihasan8@users.noreply.github.com>
2025-06-19 20:36:14 +0600

    Create N - Char.cpp

---
+++ N - Char.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+
+    if (c >= 'A' && c <= 'Z') {
+        cout << (char)(c + 32) << endl; 
+    } else  {
+        cout << (char)(c - 32) << endl; 
+        
+    } 
+    
+    return 0;
+}
