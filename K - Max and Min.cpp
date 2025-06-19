d8e6cb07983cc629ef68156f35fafacf4a9e12d5
Mehedi hasan Foysal <121812709+mehedihasan8@users.noreply.github.com>
2025-06-19 20:40:13 +0600

    Create K - Max and Min.cpp

---
+++ K - Max and Min.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    int max_num = max({a, b, c});
+    
+    int min_num = min({a, b, c});
+    
+    cout << min_num << " " << max_num;
+    
+    return 0;
+}
